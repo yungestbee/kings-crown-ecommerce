acd94d389205cf9490c15a2d24356948fea5e954
alomatee@yahoo.com <alomatee@yahoo.com>
2024-01-01 17:01:41 +0100

    folder and file setup

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Layouts from './components/Layouts';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layouts />}>
+            <Route index element={<Home />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/About" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;

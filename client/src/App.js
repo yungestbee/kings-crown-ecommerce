6d28e12200cc737f7bd79ebf8b724a93b00881be
alomatee@yahoo.com <alomatee@yahoo.com>
2024-01-05 23:53:22 +0100

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./App.css"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Layouts from './components/Layouts';
 import Home from './pages/Home';
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom'
+import {BsSearch} from 'react-icons/bs'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div
+            className="row justify-content-center align-items-center g-2"
+          >
+            <div className="col-6"><p className='text-white mb-0'>Free shipping over $100 & Free Returns</p></div>
+            <div className="col-6"><p className='text-end text-white'>Hotline: <a className='text-white' href='tel:+2349053644527'>(234) 9053 6445 27</a></p></div>
+          </div>
+          
+        </div>
+        
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h2><Link className='text-white'>King's Crown</Link></h2>
+            </div>
+            <div className="col-5">
+              <div class="input-group mb-3">
+  <input type="text" className="form-control py-2" placeholder="Search Product Here..." aria-label="Search Product Here..." aria-describedby="basic-addon2" />
+  <span className="input-group-text p-3" id="basic-addon2"><BsSearch className='fs-5'/></span>
+</div>
+            </div>
+            <div className="col-5"></div>
+          </div>
+        </div>
+      </header>
+    </>
   )
 }
 

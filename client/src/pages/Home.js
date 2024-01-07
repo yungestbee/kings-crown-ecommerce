14ba0081a760ec679c427347aa9cd6b604532cab
alomatee@yahoo.com <alomatee@yahoo.com>
2024-01-07 12:53:28 +0100

    completed a section in the landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,91 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative">
+                <img
+                  src="images/main-banner-1.jpg"
+                  alt="main banner"
+                  className="img-fluid rounded-3"
+                />
+                <div className="main-banner-content position-absolute">
+                  <h4>SUPERCHARGED FOR PROS</h4>
+                  <h5>iPad s13+ Pro</h5>
+                  <p>From $999.00 or $41.62/mo</p>
+                  <Link className="button">BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="d-flex flex-wrap justify-content-between align-items-center gap10">
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-01.jpg"
+                    alt="main banner"
+                    className="img-fluid rounded-3"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>BEST SALES</h4>
+                    <h5>Laptops Max</h5>
+                    <p>From $1699.00 or $64.62/mo</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-03.jpg"
+                    alt="main banner"
+                    className="img-fluid rounded-3"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>New arrivals</h4>
+                    <h5>Buy IPad Air</h5>
+                    <p>From $599.00 or $49.91/mo</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-02.jpg"
+                    alt="main banner"
+                    className="img-fluid rounded-3"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>15% off</h4>
+                    <h5>Smartwatch 7</h5>
+                    <p>
+                      Shop the latest band <br />
+                      styles or colors
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-04.jpg"
+                    alt="main banner"
+                    className="img-fluid rounded-3"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>free engraving</h4>
+                    <h5>Airpods Max</h5>
+                    <p>
+                      High-fidelity playback & <br />
+                      ultra-low distortion
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
